eb1a35a0a918485b3e2bc84d57eaa76d65ee27e9
mery-19 <meryam-soussi@hotmail.com>
2021-01-11 16:03:16 +0100

    dashboard

---
+++ LocationVoiture/Scripts/Script.js
@@ -0,0 +1,22 @@
+﻿
+
+$(document).ready(function () {
+    $('.sideMenuToggler').on('click', function () {
+        $('.wrapper').toggleClass('active');
+
+        console.log("clicked");
+
+    });
+
+    var adjustSidebar = function () {
+        $('.sidebar').slimScroll({
+            height: document.documentElement.clientHeight - $('.navbar').outerHeight()
+        });
+    };
+
+    adjustSidebar();
+    $(window).resize(function () {
+        adjustSidebar();
+    });
+
+});
